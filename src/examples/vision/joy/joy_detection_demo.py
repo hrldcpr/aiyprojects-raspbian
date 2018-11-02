2204f9809a51e26d7d2d963a2725b752cd905d7c
Harold Cooper <hcooper@google.com>
2018-11-02 16:10:29 -0400

    25% ROI

--- src/examples/vision/joy/joy_detection_demo.py
+++ src/examples/vision/joy/joy_detection_demo.py
@@ -255,7 +255,8 @@ class JoyDetector(object):
             # Forced sensor mode, 1640x1232, full FoV. See:
             # https://picamera.readthedocs.io/en/release-1.13/fov.html#sensor-modes
             # This is the resolution inference run on.
-            with PiCamera(sensor_mode=4, resolution=(1640, 1232)) as camera, PrivacyLed(leds):
+            # Zoom to center 25% ROI of the full image.
+            with PiCamera(sensor_mode=4, resolution=(1640, 1232), zoom=(0.375, 0.375, 0.25, 0.25)) as camera, PrivacyLed(leds):
                 def take_photo():
                     logger.info('Button pressed.')
                     player.play(BEEP_SOUND)
