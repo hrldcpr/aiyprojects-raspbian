315c3857ed13a65787dd80a0d97f15f19b9a1765
Harold Cooper <hcooper@google.com>
2018-11-13 12:20:42 -0500

    try again

--- src/examples/vision/camurai/camurai.py
+++ src/examples/vision/camurai/camurai.py
@@ -6,10 +6,19 @@ from aiy.vision.leds import Leds
 
 import common
 
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.INFO)
 
 BUTTON_PIN = 23
 SERVER_ADDRESS = '192.168.0.100'
+
+writers = []
+
+def when_pressed(pressed):
+    logging.info('pressed {}'.format(pressed))
+    if not writers:
+        logging.warning('no connection')
+        return
+    writers[0].write(1 if pressed else 0)
 
 async def led_loop(reader):
     leds = Leds()
@@ -19,18 +28,18 @@ async def led_loop(reader):
         if r == g == b == 0: leds.update(Leds.rgb_off())
         else: leds.update(Leds.rgb_on([r, g, b]))
 
-async def button_loop(writer):
-    button = Button(BUTTON_PIN)
-    button.when_pressed = lambda: writer.write(bytes([1]))
-    button.when_released = lambda: writer.write(bytes([0]))
-    await asyncio.sleep(1.0)
-
 async def main():
     reader, writer = await asyncio.open_connection(SERVER_ADDRESS, common.SERVER_PORT)
+    writers.append(writer)
     await asyncio.gather(
         led_loop(reader),
-        button_loop(writer),
+        # TODO button_loop(writer),
     )
+
+# TODO something more async / more threadsafe than this...
+button = Button(BUTTON_PIN)
+button.when_pressed = lambda: when_pressed(1)
+button.when_released = lambda: when_pressed(0)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
